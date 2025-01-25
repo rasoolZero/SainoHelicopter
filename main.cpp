1fa015985560448895d2a73bfe8a1dbea5a07137
rasoolZero <rasool.kamkar20@gmail.com>
2025-01-25 19:03:38 +0330

    increased sampling quality

--- main.cpp
+++ main.cpp
@@ -1,9 +1,14 @@
 #include <QGuiApplication>
 #include <QQmlApplicationEngine>
+#include <QSurfaceFormat>
 
 int main(int argc, char *argv[])
 {
     QGuiApplication app(argc, argv);
+
+    QSurfaceFormat format;
+    format.setSamples(8);
+    QSurfaceFormat::setDefaultFormat(format);
 
     QQmlApplicationEngine engine;
     QObject::connect(
